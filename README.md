1ee6eb66ced739a2db949238cc842dafa1a88f38
emipa606 <emil.offermo@litet.net>
2021-06-03 10:42:52 +0200

    First publish

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ReleaseWhenHealthy
+
+![Image](https://i.imgur.com/WAEzk68.png)
+
+
+Adds a prisoner-interaction to release prisoner only when healthy.
+When they get healthy the interaction is automatically set to Release.
+
+![Image](https://i.imgur.com/Rs6T6cr.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+
+
+
---
+++ Source/ReleaseWhenHealthy/Thing_AmbientTemperature.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ReleaseWhenHealthy
+{
+    [HarmonyPatch(typeof(Pawn_GuestTracker), "GuestTrackerTick")]
+    public static class Thing_AmbientTemperature
+    {
+        public static void Postfix(ref Pawn_GuestTracker __instance)
+        {
+            if (GenTicks.TicksGame % GenTicks.TickLongInterval != 0)
+            {
+                return;
+            }
+
+            if (__instance.interactionMode != ReleaseWhenHealthy.ReleaseWhenHealthyMode)
+            {
+                return;
+            }
+
+            var bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                            | BindingFlags.Static;
+            var field = typeof(Pawn_GuestTracker).GetField("pawn", bindFlags);
+
+            var currentPawn = (Pawn) field?.GetValue(__instance);
+            if (currentPawn == null)
+            {
+                return;
+            }
+
+            if (HealthAIUtility.ShouldSeekMedicalRest(currentPawn))
+            {
+                return;
+            }
+
+            __instance.interactionMode = PrisonerInteractionModeDefOf.Release;
+            var text = "RWH.pawnhealthy".Translate(currentPawn.NameFullColored);
+            var messageType = MessageTypeDefOf.PositiveEvent;
+            var message = new Message(text, messageType, new LookTargets(currentPawn));
+            Messages.Message(message);
+        }
+    }
+}
